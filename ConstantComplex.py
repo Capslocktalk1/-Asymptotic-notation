889d5590215ea88fe6660f44dd7b094798526b50
Capslocktalk <abeer242009@gmail.com>
2024-09-28 13:33:47 +0200

    first commit

---
+++ ConstantComplex.py
@@ -0,0 +1,7 @@
+def printNumber(n):
+    i = 0
+    print("The number user entered: ", n)
+    i+=1
+    print("the iteration done by this code: ", i)
+    
+printNumber(3000)
---
+++ nComplex.py
@@ -0,0 +1,8 @@
+def printNumber(n):
+    i = 0
+    print("The number user enbtered: ", n)
+    for i in range(1,n):
+        i+=1
+    print("The iteration done by this code: ", i)
+    
+printNumber(40)
